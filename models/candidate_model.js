fcb4f8cf2dd8e778d816cd8e74b5839cefb33731
rahul1346 <rahulsharma2001@gmail.com>
2015-03-16 20:14:51 -0700

    error

--- models/candidate_model.js
+++ models/candidate_model.js
@@ -1,13 +1,24 @@
 'use strict';
 
+//var models = require('../models');
 var Sequelize = require('sequelize');
 
 module.exports = function (sequelize) {
-	var Candidate = sequelize.define("candidate", {
-		username: { type: Sequelize.STRING, validate: {	notNull: true } },
-		password: { type: Sequelize.STRING, validate: { notNull: true } }
-	});
-	return {
-		Candidate: Candidate
-	}
-}
+    var Candidate = sequelize.define("candidate", {
+        name: { type: Sequelize.STRING, validate: { notNull: true } },
+        phone_number: { type: Sequelize.STRING, validate: { notNull: true } },
+        address: { type: Sequelize.STRING, validate: { notNull: true } },
+        state: { type: Sequelize.STRING, validate: { notNull: true } },
+        skills: { type: Sequelize.STRING, validate: { notNull: true } },
+        candidate_picture: { type: Sequelize.BLOB, validate: { notNull: true } },
+        // classMethods: 
+        // {
+        //     associate: 
+        //         { Candidate.belongsTo(models.User) },
+        // } 
+    });
+
+    return {
+        Candidate: Candidate
+    };
+};
--- models/employer_model.js
+++ models/employer_model.js
@@ -1,12 +1,23 @@
 'use strict';
 
+//var models = require('../models');
 var Sequelize = require('sequelize');
 
 module.exports = function (sequelize) {
     var Employer = sequelize.define("employer", {
-        username: { type: Sequelize.STRING, validate: { notNull: true } },
-        password: { type: Sequelize.STRING, validate: { notNull: true } }
+        name: { type: Sequelize.STRING, validate: { notNull: true } },
+        phone_number: { type: Sequelize.STRING, validate: { notNull: true } },
+        address: { type: Sequelize.STRING, validate: { notNull: true } },
+        state: { type: Sequelize.STRING, validate: { notNull: true } },
+        employer_description: { type: Sequelize.STRING, validate: { notNull: true} },
+        employer_picture: { type: Sequelize.STRING, validate: { notNull: true } },
+        // classMethods: 
+        // {
+        //     associate: 
+        //         { Employer.belongsTo(models.User) },      
+        // }
     });
+
     return {
         Employer: Employer
     };
