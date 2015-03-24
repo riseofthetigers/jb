e007c1b3d5360f187047613046be8be9cfab22e0
fundon <cfddream@gmail.com>
2015-03-24 23:15:38 +0800

    service: fromat

--- server.js
+++ server.js
@@ -2,35 +2,35 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var express = require("express");
+var isProduction = process.env.NODE_ENV === 'production';
 var app = express();
 
 //middleware
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(morgan('dev'));
+app.use(morgan(isProduction || 'dev'));
 // static resources
 app.use(express.static("./public"));
 
 // resources routes
 //
-// NOTE: If you like the `use` style.
+// NOTE: If you don't like the `use` style.
 // You can use of `get` `post` `put` `delete` instead of `user`.
 //
 // Example:
 //
-//	var usersController = require('./controllers/users_controller');
-// 	app.get('/user', usersController.get)
-//	app.post("/user", usersController.create);
-//	app.put("/user/:id", usersController.update);
-//	app.delete("/user/:id", usersController.destroy);
+//  var usersController = require('./controllers/users_controller');
+//  app.get('/user', usersController.get)
+//  app.post("/user", usersController.create);
+//  app.put("/user/:id", usersController.update);
+//  app.delete("/user/:id", usersController.destroy);
 //
 app.use("/user", require('./routes/user_route'));
 app.use("/employer", require('./routes/employer_route'));
 app.use("/listing", require('./routes/listing_route'));
 app.use('/candidate', require('./routes/candidate_route'));
 app.use("/business", require('./routes/business_route'));
-
 
 // Init the models
 var db = require("./models");
