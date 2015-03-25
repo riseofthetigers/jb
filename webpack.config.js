9333e54b3a01a6a2b4c509d3880da729a8773ea0
fundon <cfddream@gmail.com>
2015-03-25 18:16:08 +0800

    react: init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    filename: '[name].js',
+    publicPath: '/js/',
+    chunkFilename: "[name].js"
+  },
+
+  entry: [
+    './client/main.js'
+  ],
+
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel', 'jsx-loader'],
+      include: path.join(__dirname, 'client')
+    }]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+};
