379496b3f7e9383914492bd6e2195a11aea493d8
fundon <cfddream@gmail.com>
2015-03-30 11:10:23 +0800

    update server

--- server.js
+++ server.js
@@ -9,25 +9,34 @@ var isProduction = process.env.NODE_ENV === 'production';
 var app = express();
 
 //middleware
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 app.use(morgan(isProduction || 'dev'));
 // static resources
 app.use(express.static("./public"));
+app.use(cookieParser());
+// to support JSON-encoded bodies
+app.use(bodyParser.json());
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(methodOverride());
 
-// Use cookie middleware
-app.use(cookieParser());
 // Enable session management by express
 app.use(session({
   secret: 'changethis!',
-  resave: false,
-  saveUninitialized: false
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: true }
 }));
 
 // Init the models
 var db = require("./models");
+
+var auth_handler = require('./lib/auth/passport.js')(app, db.User);
+
+// resources routes
+app.use('/', require('./routes/web'));
+app.use('/api', auth_handler, require('./routes/api'));
 
 db.sequelize
   // You can set `force` to `true` in development mode.
@@ -39,10 +48,3 @@ db.sequelize
     console.log('Express server listening on port 5000');
     app.listen(5000);
   });
-
-
-var auth_handler = require('./lib/auth/passport.js')(app, db.User);
-
-// resources routes
-app.use('/', require('./routes/web'));
-app.use('/api', auth_handler, require('./routes/api'));
