d474d0b678ac1e15cb133e8dd90f726672c77b65
Tiberiu Craciun <tibi@happysoft.ro>
2015-07-07 20:25:16 +0300

    removed sync forst true to not delete the database

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var db = require("./models");
 db.sequelize
   //You can set `force` to `true` in development mode.
   .sync({
-    force: true
+    //force: true
     })
   //.sync()
   .then(function() {
